abbd72e78615b107ad49455e986888062dbbad3f
jamaln02 <jamal.nabaa13@gmail.com>
2024-08-05 20:09:12 +0300

    add loading and handle error

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -10,6 +10,11 @@ import {
   styled,
   tableCellClasses,
   Box,
+  CircularProgress,
+  Typography,
+  Card,
+  CardContent,
+  Alert,
 } from "@mui/material";
 import Pagination from "@mui/material/Pagination";
 import Papa from "papaparse";
@@ -23,6 +28,8 @@ const DataTable = () => {
   const [filter, setFilter] = useState("");
   const [page, setPage] = useState(1);
   const rowsPerPage = 10;
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
@@ -49,6 +56,7 @@ const DataTable = () => {
 
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
       try {
         const response = await fetch("/data.csv");
         if (!response.ok) {
@@ -60,7 +68,9 @@ const DataTable = () => {
         setData(result.data);
         setFilteredData(result.data);
       } catch (error) {
-        console.error("Error fetching or parsing data:", error);
+        setError(error.message);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -80,64 +90,87 @@ const DataTable = () => {
     setFilteredData(filtered);
   }, [search, filter, data]);
   return (
-    <Paper>
-      <Filter
-        search={search}
-        setSearch={setSearch}
-        filter={filter}
-        setFilter={setFilter}
-      />
-
-      <TableContainer>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <StyledTableCell>Created_DT</StyledTableCell>
-              <StyledTableCell>Modified_DT</StyledTableCell>
-              <StyledTableCell>Entity</StyledTableCell>
-              <StyledTableCell>Operating Status </StyledTableCell>
-              <StyledTableCell>Legal Name</StyledTableCell>
-              <StyledTableCell>DBA Name</StyledTableCell>
-              <StyledTableCell>Physical Address</StyledTableCell>
-              <StyledTableCell>Phone</StyledTableCell>
-              <StyledTableCell>DOT</StyledTableCell>
-              <StyledTableCell>MC/MX/FF</StyledTableCell>
-              <StyledTableCell>Power Units</StyledTableCell>
-              <StyledTableCell>Out of Service Date</StyledTableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {filteredData
-              .slice((page - 1) * rowsPerPage, page * rowsPerPage)
-              .map((row, index) => (
-                <StyledTableRow key={index}>
-                  <StyledTableCell>{row.created_dt}</StyledTableCell>
-                  <StyledTableCell>
-                    {row.data_source_modified_dt}
-                  </StyledTableCell>
-                  <StyledTableCell>{row.entity_type}</StyledTableCell>
-                  <StyledTableCell>{row.record_status}</StyledTableCell>
-                  <StyledTableCell>{row.legal_name}</StyledTableCell>
-                  <StyledTableCell>{row.dba_name}</StyledTableCell>
-                  <StyledTableCell>{row.physical_address}</StyledTableCell>
-                  <StyledTableCell>{row.phone}</StyledTableCell>
-                  <StyledTableCell>{row.usdot_number}</StyledTableCell>
-                  <StyledTableCell>{row.mc_mx_ff_number}</StyledTableCell>
-                  <StyledTableCell>{row.power_units}</StyledTableCell>
-                  <StyledTableCell>{row.out_of_service_date}</StyledTableCell>
-                </StyledTableRow>
-              ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <Box mt={2} display="flex" justifyContent="center" marginBottom={4}>
-        <Pagination
-          count={Math.ceil(filteredData.length / rowsPerPage)}
-          page={page}
-          onChange={(event, value) => setPage(value)}
-        />
-      </Box>
-    </Paper>
+    <Box>
+      <Card>
+        <CardContent>
+          <Filter
+            search={search}
+            setSearch={setSearch}
+            filter={filter}
+            setFilter={setFilter}
+          />
+          {loading ? (
+            <Box
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              height={300}
+            >
+              <CircularProgress />
+              <Typography variant="h6" component="div" marginLeft={2}>
+                Loading...
+              </Typography>
+            </Box>
+          ) : error ? (
+            <Alert severity="error">{error}</Alert>
+          ) : (
+            <TableContainer>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <StyledTableCell>Created_DT</StyledTableCell>
+                    <StyledTableCell>Modified_DT</StyledTableCell>
+                    <StyledTableCell>Entity</StyledTableCell>
+                    <StyledTableCell>Operating Status </StyledTableCell>
+                    <StyledTableCell>Legal Name</StyledTableCell>
+                    <StyledTableCell>DBA Name</StyledTableCell>
+                    <StyledTableCell>Physical Address</StyledTableCell>
+                    <StyledTableCell>Phone</StyledTableCell>
+                    <StyledTableCell>DOT</StyledTableCell>
+                    <StyledTableCell>MC/MX/FF</StyledTableCell>
+                    <StyledTableCell>Power Units</StyledTableCell>
+                    <StyledTableCell>Out of Service Date</StyledTableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {filteredData
+                    .slice((page - 1) * rowsPerPage, page * rowsPerPage)
+                    .map((row, index) => (
+                      <StyledTableRow key={index}>
+                        <StyledTableCell>{row.created_dt}</StyledTableCell>
+                        <StyledTableCell>
+                          {row.data_source_modified_dt}
+                        </StyledTableCell>
+                        <StyledTableCell>{row.entity_type}</StyledTableCell>
+                        <StyledTableCell>{row.record_status}</StyledTableCell>
+                        <StyledTableCell>{row.legal_name}</StyledTableCell>
+                        <StyledTableCell>{row.dba_name}</StyledTableCell>
+                        <StyledTableCell>
+                          {row.physical_address}
+                        </StyledTableCell>
+                        <StyledTableCell>{row.phone}</StyledTableCell>
+                        <StyledTableCell>{row.usdot_number}</StyledTableCell>
+                        <StyledTableCell>{row.mc_mx_ff_number}</StyledTableCell>
+                        <StyledTableCell>{row.power_units}</StyledTableCell>
+                        <StyledTableCell>
+                          {row.out_of_service_date}
+                        </StyledTableCell>
+                      </StyledTableRow>
+                    ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          )}
+          <Box my={2} display="flex" justifyContent="center">
+            <Pagination
+              count={Math.ceil(filteredData.length / rowsPerPage)}
+              page={page}
+              onChange={(event, value) => setPage(value)}
+            />
+          </Box>
+        </CardContent>
+      </Card>
+    </Box>
   );
 };
 
